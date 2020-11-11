725e3434e13e199c30292bead2b122b89515a64b
Daren McCulley <mcculleydj@gmail.com>
2020-11-11 16:11:45 -0500

    no deploy

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Building and deploying the Vue application..."
-yarn run build
-scp -r -i ~/.ssh/mcculleydj.dev.pem dist ubuntu@54.203.119.188:/home/ubuntu/d3
-ssh -i ~/.ssh/mcculleydj.dev.pem ubuntu@54.203.119.188 sudo systemctl restart nginx
